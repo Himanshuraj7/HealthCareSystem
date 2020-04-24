ef7ec458dc935f71b233ea37c8e8977bc1af8a02
VISHAL <vishal162261@gmail.com>
2020-04-24 06:06:49 +0530

    deleted test project

--- HealthCareSystem/src/main/java/com/capgemini/app/repositery/UserRepositery.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.app.repositery;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.capgemini.app.entity.User;
-@Repository
-public interface UserRepositery extends JpaRepository<User,String>{
-
-}
