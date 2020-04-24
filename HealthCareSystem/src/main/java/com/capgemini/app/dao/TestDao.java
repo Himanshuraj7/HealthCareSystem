3aaf42d2945afff55a141837b783e7eb1059be68
VISHAL <vishal162261@gmail.com>
2020-04-24 06:50:00 +0530

    test dao

--- HealthCareSystem/src/main/java/com/capgemini/app/dao/TestDao.java
+++ HealthCareSystem/src/main/java/com/capgemini/app/dao/TestDao.java
@@ -1,6 +1,15 @@
 package com.capgemini.app.dao;
 
+import java.util.List;
+
+import com.capgemini.app.entities.Test;
+
 public interface TestDao {
 
+	boolean addTest(Test test);
+	boolean removeTest(long testId);
+	List<Test> getAllTests();
+	Test getTest(long testId);
+	
 	
 }
--- HealthCareSystem/src/main/java/com/capgemini/app/dao/TestDaoImpl.java
+++ HealthCareSystem/src/main/java/com/capgemini/app/dao/TestDaoImpl.java
@@ -1,5 +1,48 @@
 package com.capgemini.app.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.app.entities.Test;
+
+@Repository
 public class TestDaoImpl implements TestDao {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public boolean addTest(Test test) {
+		em.persist(test);
+		return false;
+	}
+
+	@Override
+	public boolean removeTest(long testId) {
+		Test test = em.find(Test.class, testId);
+		if(test!=null)
+		{
+			em.remove(test);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public List<Test> getAllTests() {
+		TypedQuery<Test> query = em.createQuery("from TEST", Test.class);
+		return query.getResultList();
+	}
+
+	@Override
+	public Test getTest(long testId) {
+		Test test = em.find(Test.class, testId);
+		return test;
+	}
 
 }
